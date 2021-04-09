7fde202bb212c6627738c034cb646ee4ee9a9c9d
pvgorshenin <mr.gorshenya.job@gmail.com>
2021-04-09 20:05:58 +0300

    logging simplified

--- logging_utils.py
+++ logging_utils.py
@@ -2,6 +2,21 @@ from logger import XGBLogger
 from functools import wraps
 
 
+
+def _save_val_preds(logger, preds_val, booster_params, kfold, i_fold):
+    '''
+    Сохраняем по эпохам для мультикласса, иначе целиком
+
+    :param preds_val:  n_splits * n_examles * n_targets для мультикласса с выходом n_examles * n_targets
+                        n_splits * n_examles в других случаях
+    '''
+
+    if booster_params['objective'] == 'multi:softprob':
+        logger.save_val_preds(preds_val[i_fold], i_fold)
+    elif i_fold == kfold.n_splits-1:
+        logger.save_val_preds(preds_val, 0)
+
+
 def common_logging(func):
     @wraps(func)
     def wrapper(*args, **kwargs):
@@ -9,9 +24,14 @@ def common_logging(func):
                            description=kwargs['log_params']['description'])
         logger._make_resultdir_n_subdirs()
         logger.save_description()
-        kwargs['logger'] = logger
 
-        preds_train, preds_val = func(*args, **kwargs)
+        preds_train, preds_val, model_lst = func(*args, **kwargs)
+
+        for i_fold in range(kwargs['kfold'].n_splits):
+            _save_val_preds(logger, preds_val, kwargs['booster_params'], kwargs['kfold'], i_fold)
+            logger.save_model(model_lst[i_fold], i_fold)
+            if (kwargs['val'] is not None) & (kwargs['val_labels'] is not None):
+                logger.calc_metric(kwargs['metric'], preds_val[i_fold, :, :], kwargs['val_labels'].values)
 
         logger.save_train_preds(preds_train)
         logger.save_params(kwargs['booster_params'], kwargs['train_params'], kwargs['log_params'])
--- run_xgb.py
+++ run_xgb.py
@@ -8,6 +8,7 @@ from logging_utils import common_logging
 
 def _get_init_data(train, val, train_cols,  kfold, booster_params):
     preds_train = np.zeros(train.shape[0])
+    model_lst = []
     if val is not None:
         if isinstance(val, pd.DataFrame):
             dval = xgb.DMatrix(val[train_cols])
@@ -16,10 +17,10 @@ def _get_init_data(train, val, train_cols,  kfold, booster_params):
         if booster_params['objective'] == 'multi:softprob':
             preds_val = np.zeros([kfold.n_splits, val.shape[0], booster_params['num_class']])
             preds_train = np.zeros([train.shape[0], booster_params['num_class']])
-            return (preds_train, dval, preds_val)
+            return (preds_train, model_lst, dval, preds_val)
         preds_val = np.zeros([kfold.n_splits, val.shape[0]])
-        return (preds_train, dval, preds_val)
-    return (preds_train, None, None)
+        return (preds_train, model_lst, dval, preds_val)
+    return (preds_train, model_lst, None, None)
 
 
 def _get_kfold_data(train, train_index, test_index, train_cols, labels):
@@ -32,26 +33,11 @@ def _get_kfold_data(train, train_index, test_index, train_cols, labels):
     return dtrain_train, dtrain_val
 
 
-def _predict_n_save_val(preds_val: np.ndarray, xgb_model, dval, logger, booster_params, kfold, i_fold):
-    '''
-    Сохраняем по эпохам для мультикласса, иначе целиком
-
-    :param preds_val:  n_splits * n_examles * n_targets для мультикласса с выходом n_examles * n_targets
-                        n_splits * n_examles в других случаях
-    '''
-    preds_val[i_fold] = xgb_model.predict(dval)
-    if booster_params['objective'] == 'multi:softprob':
-        logger.save_val_preds(preds_val[i_fold], i_fold)
-    elif i_fold == kfold.n_splits-1:
-        logger.save_val_preds(preds_val, 0)
-    return preds_val
-
-
 @common_logging
 def run_xgb(train: Union[pd.DataFrame, csr_matrix], val: Union[pd.DataFrame, csr_matrix], train_cols: list,
             train_labels: Union[pd.DataFrame, pd.Series], val_labels: Union[pd.DataFrame, pd.Series],
             booster_params: dict, train_params: dict, log_params: dict,
-            kfold, metric, logger) -> Tuple[np.ndarray]:
+            kfold, metric) -> Tuple[np.ndarray]:
     """
     Runs xgboost in KFold cycle
 
@@ -61,7 +47,7 @@ def run_xgb(train: Union[pd.DataFrame, csr_matrix], val: Union[pd.DataFrame, csr
     :param logger: always None, overwritten in common_logging function
     :return: preds_train, preds_val
     """
-    preds_train, dval, preds_val = _get_init_data(train, val, train_cols, kfold, booster_params)
+    preds_train, model_lst, dval, preds_val = _get_init_data(train, val, train_cols, kfold, booster_params)
     for i_fold, (train_index, test_index) in enumerate(kfold.split(train)):
         dtrain_train, dtrain_val = _get_kfold_data(train, train_index, test_index, train_cols, train_labels)
         evallist = [(dtrain_train, 'train'), (dtrain_val, 'val')]
@@ -71,10 +57,8 @@ def run_xgb(train: Union[pd.DataFrame, csr_matrix], val: Union[pd.DataFrame, csr
                               evals=evallist,
                               verbose_eval=train_params['verbose_eval'],
                               early_stopping_rounds=train_params['early_stopping_rounds'])
-        logger.save_model(xgb_model, i_fold)
+        model_lst.append(xgb_model)
         preds_train[test_index] = xgb_model.predict(dtrain_val)
         if val is not None:
-            preds_val = _predict_n_save_val(preds_val, xgb_model, dval, logger, booster_params, kfold, i_fold)
-            if val_labels is not None:
-                logger.calc_metric(metric, preds_val[i_fold, :, :], val_labels.values)
-    return (preds_train, preds_val)
+            preds_val[i_fold] = xgb_model.predict(dval)
+    return (preds_train, preds_val, model_lst)
